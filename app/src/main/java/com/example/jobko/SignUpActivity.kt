22f7e7db1b4a4906733c9278f394e0eca5284f28
Animesh Roy <royanimesh2211@gmail.com>
2026-03-27 13:30:43 +0600

    set mandatory field * red color

--- app/src/main/java/com/example/jobko/SignUpActivity.kt
+++ app/src/main/java/com/example/jobko/SignUpActivity.kt
@@ -1,12 +1,21 @@
 package com.example.jobko
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var fullName: TextView
+    val text = "Full Name *"
+    val spannable = SpannableString(text)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,17 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fullName = findViewById(R.id.txtFullName)
+
+        spannable.setSpan(
+            ForegroundColorSpan(Color.RED),
+            text.length - 1,
+            text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        fullName.text = spannable
+
     }
 }
