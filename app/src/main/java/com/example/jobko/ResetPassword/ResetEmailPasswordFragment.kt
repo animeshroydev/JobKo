d6698b4abc7959d95bacfe15e43e19c6b8be2f5c
Animesh Roy <royanimesh2211@gmail.com>
2026-03-30 10:16:24 +0530

    opened verification PIN fragment on btn click

--- app/src/main/java/com/example/jobko/ResetPassword/ResetEmailPasswordFragment.kt
+++ app/src/main/java/com/example/jobko/ResetPassword/ResetEmailPasswordFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.jobko.R
 
 class ResetEmailPasswordFragment : Fragment() {
 
+    lateinit var btnSignIn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,8 +19,27 @@ class ResetEmailPasswordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reset_email, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSignIn = view.findViewById(R.id.btnSignIn)
+        btnSignIn.setOnClickListener {
+
+            val fragment = VerificationPinFragment()
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .addToBackStack(null)
+                .commit()
+
+        }
+
     }
 
 }
