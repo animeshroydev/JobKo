fc517dfd8bdd3a0f506f045672c5108fddfe30b6
Animesh Roy <royanimesh2211@gmail.com>
2026-03-31 18:01:42 +0530

    Add gender dropdown and DOB date picker

--- app/src/main/java/com/example/jobko/SetupProfile/SetupProfileFragment.kt
+++ app/src/main/java/com/example/jobko/SetupProfile/SetupProfileFragment.kt
@@ -1,11 +1,18 @@
 package com.example.jobko.SetupProfile
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import com.example.jobko.R
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class SetupProfileFragment : Fragment() {
 
@@ -22,5 +29,39 @@ class SetupProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val genderField = view.findViewById<AutoCompleteTextView>(R.id.edtGender)
+        val dobField = view.findViewById<EditText>(R.id.edtDateOfBirth)
+
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            arrayOf("Male", "Female")
+        )
+
+        genderField.setAdapter(adapter)
+        genderField.setOnClickListener { genderField.showDropDown() }
+
+        dobField.setOnClickListener {
+            val calendar = Calendar.getInstance()
+
+            val datePicker = DatePickerDialog(
+                requireContext(),
+                { _, year, month, day ->
+                    val selected = Calendar.getInstance()
+                    selected.set(year, month, day)
+
+                    val sdf = SimpleDateFormat("dd MMMM, yyyy", Locale.getDefault())
+                    dobField.setText(sdf.format(selected.time))
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+
+            datePicker.show()
+        }
+
     }
 }
