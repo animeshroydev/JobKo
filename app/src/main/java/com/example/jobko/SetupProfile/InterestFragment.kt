b9277b1ba9c493d05fced8837c3d0161198afdb1
Animesh Roy <royanimesh2211@gmail.com>
2026-04-05 17:24:20 +0530

    dark mode

--- app/src/main/java/com/example/jobko/SetupProfile/InterestFragment.kt
+++ app/src/main/java/com/example/jobko/SetupProfile/InterestFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.jobko.R
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
 
 class InterestFragment : Fragment() {
 
+    lateinit var continueBtn: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,6 +27,18 @@ class InterestFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        continueBtn = view.findViewById(R.id.continueBtn)
+        continueBtn.setOnClickListener {
+
+            val fragment = ConfirmNewAccountFragment()
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
     }
 
 }
